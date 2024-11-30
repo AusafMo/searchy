23fcb4ecdfed494207b3e53b728c1b5442014fcd
Mohammad Ausaf <75237046+AusafMo@users.noreply.github.com>
2024-12-01 00:37:53 +0530

    Initial Commit

---
+++ searchy/searchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  searchyApp.swift
+//  searchy
+//
+//  Created by Mohammad Ausaf on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct searchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
